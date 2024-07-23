de229645320f9745f06ef7b4130e50c264eebc0f
Erin <erincdustin@gmail.com>
2024-07-23 14:45:58 -0700

    Milestone 2 Fast follows

--- apps/admin/src/components/ResourceList/FilterSearchMenu.tsx
+++ apps/admin/src/components/ResourceList/FilterSearchMenu.tsx
@@ -24,12 +24,13 @@ import {
 import { title } from 'case'
 import { get } from 'lodash'
 import pluralize from 'pluralize'
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 import { TbPlus, TbSearch } from 'react-icons/tb'
 import { DebouncedInput } from '../Shared/DebouncedInput'
 import { ColumnFiltersState } from '@tanstack/react-table'
 import { ServiceListOptions } from './ListView'
 import { OpenAPIV3 } from 'openapi-types'
+import { getHeaderNameOverride } from '../../utils/spec.utils'
 
 interface FilterSearchMenuProps {
   resourceName: string
@@ -39,7 +40,8 @@ interface FilterSearchMenuProps {
   properties: { [name: string]: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject } | undefined
   handleRoutingChange: (
     queryKey: string,
-    resetPage?: boolean
+    resetPage?: boolean,
+    index?: number
   ) => (value?: string | boolean | number) => void
 }
 
@@ -54,6 +56,46 @@ const FilterSearchMenu: FC<FilterSearchMenuProps> = ({
   const subtleIcon = useColorModeValue('blackAlpha.500', 'whiteAlpha.500')
   const [newFilterAccessor, setNewFilterAccessor] = useState<string>('ID')
   const [newFilterValue, setNewFilterValue] = useState<string | number | undefined>('')
+
+  const getLabel = useCallback(
+    (id: string) => {
+      let override = getHeaderNameOverride(id, resourceName)
+      if (!override) return id
+
+      const segments = id.split('.')
+      if (segments.length > 1) {
+        segments.pop()
+        override = `${segments}.${override}`
+      }
+
+      return override
+    },
+    [resourceName]
+  )
+
+  const getFilterTags = useCallback(() => {
+    return (
+      <Box mb={1}>
+        {columnFilters.map((cf: any, idx) => (
+          <Tag
+            key={idx}
+            mr={1}
+            mb={1}
+            borderRadius="full"
+            colorScheme="primary"
+          >
+            <TagLabel>{`${getLabel(cf.id)}=${cf.value}`}</TagLabel>
+            <TagCloseButton
+              onClick={() => {
+                const index = columnFilters.filter((c) => c.id === cf.id).indexOf(cf)
+                handleRoutingChange(cf.id, true, index)(undefined)
+              }}
+            />
+          </Tag>
+        ))}
+      </Box>
+    )
+  }, [columnFilters, getLabel, handleRoutingChange])
 
   return (
     <>
@@ -117,57 +159,36 @@ const FilterSearchMenu: FC<FilterSearchMenuProps> = ({
               : `Filter by ${pluralize.singular(title(resourceName))} properties`}
           </PopoverHeader>
           <PopoverBody>
-            {columnFilters.length > 0 && (
-              <Box mb={1}>
-                {columnFilters.map((cf) => (
-                  <Tag
-                    key={cf.id}
-                    mr={1}
-                    mb={1}
-                    borderRadius="full"
-                    colorScheme="primary"
-                  >
-                    <TagLabel>{`${cf.id}=${cf.value}`}</TagLabel>
-                    <TagCloseButton onClick={() => handleRoutingChange(cf.id, true)(undefined)} />
-                  </Tag>
-                ))}
-              </Box>
-            )}
+            {columnFilters.length > 0 && getFilterTags()}
             <HStack>
               <Select
                 value={newFilterAccessor}
                 onChange={(e) => setNewFilterAccessor(e.target.value)}
               >
-                {columnHeaders?.map((h: string) => {
+                {columnHeaders?.filter(k => k !== 'xp').map((h: string) => {
                   const schema = get(properties, h) as any
                   if (schema?.allOf || schema.properties) {
                     const properties = schema?.allOf
                       ? schema.allOf[0].properties
                       : schema?.properties
-                    return Object.keys(properties).map((k) => {
+                    return Object.keys(properties).filter(k => k !== 'Password' && k !== 'xp').map((k) => {
                       const tid = `${h}.${k}`
-                      if (columnFilters.findIndex((c) => c.id === tid) > -1) {
-                        return null
-                      }
                       return (
                         <option
                           key={tid}
                           value={tid}
                         >
-                          {tid}
+                          {getLabel(tid)}
                         </option>
                       )
                     })
-                  }
-                  if (columnFilters.findIndex((c) => c.id === h) > -1) {
-                    return null
                   }
                   return (
                     <option
                       key={h}
                       value={h}
                     >
-                      {h}
+                      {getLabel(h)}
                     </option>
                   )
                 })}
