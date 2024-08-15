b281986e812c2c2885d70967478722178f19e09d
Robert Watt <rob.watt@sitecore.com>
2024-08-15 15:03:44 -0500

    OCP-996 - Eric's updates from Azure repo
    This includes UIs for forgot password/username and password reset by verification code. Still needs some TLC, we will get to that later, for now, it at least will fire off the requests - but the message sender would need to be configured for blob storage for us to pull those verification codes out.

---
+++ apps/admin/src/components/Authentication/ForgotPasswordVerificationForm.tsx
@@ -0,0 +1,83 @@
+import { Button, VStack } from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { ForgottenPassword } from 'ordercloud-javascript-sdk'
+import { FC, useMemo } from 'react'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { InputControl } from '../OperationForm/Controls'
+import { CLIENT_ID } from '../../constants/constants'
+
+interface IForgotPasswordVerificationForm {
+  onSuccess: () => void
+}
+
+interface FormInputs {
+  verificationCode: string
+  username: string
+  password: string
+}
+
+const ForgotPasswordVerificationForm: FC<IForgotPasswordVerificationForm> = ({ onSuccess }) => {
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      verificationCode: yup.string().required(),
+      username: yup.string().required(),
+      password: yup.string().required(),
+    })
+  }, [])
+
+  const methods = useForm<FormInputs>({ resolver: yupResolver(validationSchema) })
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+
+    await ForgottenPassword.ResetPasswordByVerificationCode(data.verificationCode!, {
+      ClientID: CLIENT_ID,
+      Username: data.username,
+      Password: data.password
+    })
+    
+    onSuccess()
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <VStack
+        as="form"
+        id="OC_FORGOT_PASSWORD_VERFICIATION_FORM"
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
+        <InputControl
+          name="verificationCode"
+          label="Verification Code"
+          isRequired
+          inputProps={{ type: 'text', placeholder: 'Verification Code' }}
+        />
+        <InputControl
+          name="username"
+          label="Username"
+          isRequired
+          inputProps={{ type: 'text', placeholder: 'Username' }}
+        />
+        <InputControl
+          name="password"
+          label="Password"
+          isRequired
+          inputProps={{ type: 'text', placeholder: 'New Password' }}
+        />
+        <Button
+          alignSelf="flex-end"
+          mt="auto"
+          px={6}
+          isDisabled={methods.formState.isLoading || !methods.formState.isValid}
+          type="submit"
+          variant="solid"
+          colorScheme="teal"
+        >
+          Verify
+        </Button>
+      </VStack>
+    </FormProvider>
+  )
+}
+
+export default ForgotPasswordVerificationForm
---
+++ apps/admin/src/components/Authentication/ForgotUsernameForm.tsx
@@ -0,0 +1,67 @@
+import { Button, VStack } from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { ForgottenPassword } from 'ordercloud-javascript-sdk'
+import { FC, useMemo } from 'react'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { InputControl } from '../OperationForm/Controls'
+import { CLIENT_ID } from '../../constants/constants'
+
+interface ILoginForm {
+  onSuccess: () => void
+}
+
+interface FormInputs {
+  email?: string
+}
+
+const ForgotUsernameForm: FC<ILoginForm> = ({ onSuccess }) => {
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      email: yup
+        .string()
+        .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, 'Invalid email format'),
+    })
+  }, [])
+
+  const methods = useForm<FormInputs>({ resolver: yupResolver(validationSchema) })
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    await ForgottenPassword.SendVerificationCode({
+      ClientID: CLIENT_ID,
+      Email: data.email,
+    })
+    onSuccess()
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <VStack
+        as="form"
+        id="OC_FORGOT_PASSWORD_FORM"
+        noValidate
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
+        <InputControl
+          name="email"
+          isRequired={true}
+          label="Email"
+          inputProps={{ type: 'text', placeholder: 'Email' }}
+          control={methods.control}
+        />
+        <Button
+          alignSelf="flex-end"
+          mt="auto"
+          isDisabled={methods.formState.isLoading || !methods.formState.isValid}
+          type="submit"
+          variant="solid"
+          colorScheme="teal"
+        >
+          Send Link
+        </Button>
+      </VStack>
+    </FormProvider>
+  )
+}
+
+export default ForgotUsernameForm
