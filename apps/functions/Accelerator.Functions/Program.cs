913e249f4c8a8f73841fa1ff5ed08fac616ca649
DJ Steinmetz <steinmetz.dj@gmail.com>
2025-02-04 14:36:20 -0600

    OCP-1131 update apps/functions to .net from node

---
+++ apps/functions/Accelerator.Functions/Program.cs
@@ -0,0 +1,19 @@
+using Accelerator.Commands;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder
+    .ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+// Add Services
+builder.Services.AddSingleton(typeof(GreetingCommand));
+
+builder.Build().Run();
